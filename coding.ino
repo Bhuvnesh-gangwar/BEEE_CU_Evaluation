0fcd3b7ab6e97538e5d1da786c31106304ac152f
Bhuvnesh Gangwar <53831868+Bhuvnesh-gangwar@users.noreply.github.com>
2019-10-21 10:25:30 -0700

    Add files via upload

---
+++ coding.ino
@@ -0,0 +1,61 @@
+void setup() 
+{
+  
+// put your setup code here, to run once:
+
+
+pinMode(13,OUTPUT);
+
+
+Serial.begin(9600);
+
+
+
+}
+
+
+
+void loop() 
+
+{
+
+  // put your main code here, to run repeatedly:
+
+  
+   int d=analogRead(A0);
+
+
+     Serial.println(d);
+
+
+     delay(1000);    
+
+
+     if(d>600)
+{
+  
+
+       digitalWrite(13,HIGH);
+  
+
+       delay(100);
+  
+
+       digitalWrite(13,LOW);
+  
+
+       delay(100);
+
+     }
+
+
+     else 
+{
+  
+
+           digitalWrite(13,LOW);
+
+
+          }
+
+}
