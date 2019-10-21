531ec260ea771b1f7d9baf233d3c3d30df165ff9
Bhuvnesh Gangwar <53831868+Bhuvnesh-gangwar@users.noreply.github.com>
2019-10-21 11:12:00 -0700

    Update coding.ino

--- coding.ino
+++ coding.ino
@@ -1,61 +1,24 @@
 void setup() 
 {
-  
-// put your setup code here, to run once:
-
-
+  // put your setup code here, to run once:
 pinMode(13,OUTPUT);
-
-
 Serial.begin(9600);
-
-
-
 }
 
-
-
 void loop() 
-
-{
-
-  // put your main code here, to run repeatedly:
-
-  
-   int d=analogRead(A0);
-
-
+{  // put your main code here, to run repeatedly:
+  int d=analogRead(A0);
      Serial.println(d);
-
-
      delay(1000);    
-
-
-     if(d>600)
+     if(d>300)
 {
-  
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(100);
 
-       digitalWrite(13,HIGH);
-  
-
-       delay(100);
-  
-
-       digitalWrite(13,LOW);
-  
-
-       delay(100);
-
-     }
-
-
-     else 
-{
-  
-
-           digitalWrite(13,LOW);
-
-
-          }
-
+ }
+  else {
+            digitalWrite(13,LOW);
+       }
 }
